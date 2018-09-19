dbfee1364432bdc1c3b0af0c212ae5811bc5fb92
Manny Navarro <manny.navarro@gmail.com>
2018-09-19 14:00:33 -0700

    added For loop, output display, and console log to validate output

---
+++ Looping/loopThroughObject.js
@@ -0,0 +1,25 @@
+'use strict';
+var data = {
+    name:'Manny Navarro',
+    title: 'Instructor',
+    courses: {
+        php:'https://mannynavarro.com',
+        javascript:'https://mannynavarro.com',
+        youtube:'https://mannynavarro.com',
+        twitter:'https://mannynavarro.com',
+        facebook: 'https://mannynavarro.com'
+    }
+};
+
+/*var output = "";
+    for(var key in data.courses){
+        //console.log(key);
+        output += '<li>' +
+        '<a href="' + data.courses[key] +
+         '">' + key + '</a>' +
+         '</li>';
+    }
+*/
+    //var update = document.getElementById('social');
+
+    //update.innerHTML = output;
