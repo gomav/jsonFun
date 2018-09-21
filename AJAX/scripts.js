d3de99ba7cbe82cdd98cbe3c11b9d2f1e8013568
Manny Navarro <manny.navarro@gmail.com>
2018-09-20 18:14:42 -0700

    AJAX and JSON pratice, not completely working yet, needs further trouble shooting

--- AJAX/scripts.js
+++ AJAX/scripts.js
@@ -10,10 +10,29 @@ if(window.XMLHttpRequest){
 
 r.open('GET', 'data.json');
 
-r.onreadystatechange = function (){
-    if(r.status === 200) && (r.readyState === 4) {
+r.onreadystatechange = function(){
+    if(r.status === 200 && r.readyState === 4){
 
+        var data = JSON.parse(r.respondText);
+        var output = "";
+
+        for(var i = 0; i <= data.courses.length; i++){
+
+            for(var key in data.courses[i]){
+
+                if(data.courses[i].hasOwnProperty(key)){
+
+                    output += '<li>' +
+                    '<a href="' + data.courses[i][key] +
+                    '">' + key + '</a>' +
+                    '</li>';
+                }
+            }
+        }
+
+        var update = document.getElementById('social');
+        update.innerHTML = output;
     }
-
-
-}
+};
+//readyState event handler
+r.send();
