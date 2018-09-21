2cbafedb42735a00bcf1689fd543a4750b115093
Manny Navarro <manny.navarro@gmail.com>
2018-09-20 18:46:16 -0700

    All is working as expected, just needed to run server to host files and provide the expected response

--- AJAX/scripts.js
+++ AJAX/scripts.js
@@ -1,38 +1,62 @@
 'use strict';
 var r;
 
-if(window.XMLHttpRequest){
+if(window.XMLHttpRequest) {
+
     r = new XMLHttpRequest();
 
+
 } else {
-    r = new ActiveXObject('Microsoft.XMHTTP');
+
+    r = new ActiveXObject('Microsoft.XMLHTTP');
+
 }
+
 
 r.open('GET', 'data.json');
 
 r.onreadystatechange = function(){
-    if(r.status === 200 && r.readyState === 4){
 
-        var data = JSON.parse(r.respondText);
-        var output = "";
+    if(r.status === 200 && r.readyState === 4) {
 
-        for(var i = 0; i <= data.courses.length; i++){
 
-            for(var key in data.courses[i]){
+        var data = JSON.parse(r.responseText);
 
-                if(data.courses[i].hasOwnProperty(key)){
+         var output = "";
 
-                    output += '<li>' +
-                    '<a href="' + data.courses[i][key] +
-                    '">' + key + '</a>' +
-                    '</li>';
-                }
-            }
-        }
 
-        var update = document.getElementById('social');
-        update.innerHTML = output;
+           for(var i = 0; i <= data.courses.length; i++){
+
+               for(var key in data.courses[i]) {
+
+                   if(data.courses[i].hasOwnProperty(key)){
+
+                        output += '<li>' +
+                        '<a href="' + data.courses[i][key] +
+                        '">' + key + '</a>' +
+                        '</li>';
+
+
+                   }
+
+
+
+               }
+
+
+
+           }
+
+
+
+       var update = document.getElementById('social');
+
+       update.innerHTML = output;
+
+
     }
+
+
 };
-//readyState event handler
+
 r.send();
